6be910d9842eaa1de1f98795cd562ad983b9de39
sammekekko <imneuriong@gmail.com>
2020-05-11 16:47:22 +0200

    First - Start

---
+++ src/main/java/me/sammekekko/tradesystem/Main.java
@@ -0,0 +1,18 @@
+package me.sammekekko.tradesystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("TradeSystem Initialized");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        System.out.println("TradeSystem has been disabled");
+    }
+}
