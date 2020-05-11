6d5de10f45037ca09de21f48c7db7d46483c9bda
sammekekko <imneuriong@gmail.com>
2020-05-11 16:51:12 +0200

    First - Test

--- src/main/java/me/sammekekko/tradesystem/Main.java
+++ src/main/java/me/sammekekko/tradesystem/Main.java
@@ -7,7 +7,7 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("TradeSystem Initialized");
+        System.out.println("TradeSystem Initializeds");
     }
 
     @Override
